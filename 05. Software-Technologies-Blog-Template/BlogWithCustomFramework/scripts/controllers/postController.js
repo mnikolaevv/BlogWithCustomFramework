8ff7b3fa3c78479effcb0214a1056b5d849b0282
mnikolaevv <martin_n_96@abv.bg>
2016-09-01 02:04:26 +0300

    Add files via upload

---
+++ 05. Software-Technologies-Blog-Template/BlogWithCustomFramework/scripts/controllers/postController.js
@@ -0,0 +1,21 @@
+class PostController {
+    constructor(postView, requester, baseUrl, appKey) {
+        this._postView = postView;
+        this._requester = requester;
+        this._appKey = appKey;
+        this._baseServiceUrl = baseUrl + "/appdata/" + appKey + "/posts";
+    }
+    showCreatePostPage(fullName, isLoggedIn) {
+        this._postView.showCreatePostPage(fullName, isLoggedIn);
+    }
+    createNewPost(data) {
+        this._requester.post(this._baseServiceUrl, data, function(responseData) {
+            showPopup('success', 'Post created successfully.');
+            redirectUrl('#/');
+        }, function(responseData) {
+            showPopup('error', 'Post not created.');
+            
+            
+        })
+    }
+}
